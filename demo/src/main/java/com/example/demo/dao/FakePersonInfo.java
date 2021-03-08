029402f7e3865c5c24802f4552f1f2d980f87eb9
KacperGielda <mateuszhoch834@gmail.com>
2021-03-08 12:17:36 +0100

    Start of App

--- demo/src/main/java/com/example/demo/dao/FakePersonInfo.java
+++ demo/src/main/java/com/example/demo/dao/FakePersonInfo.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-@Repository
+@Repository("fakeDAO")
 public class FakePersonInfo implements PersonDao{
     private static List<Person> DB = new ArrayList<>();
     @Override
