bf2dbcb50f9acadafc4a15e608888ef51d7451cc
KacperGielda <mateuszhoch834@gmail.com>
2021-03-08 12:49:49 +0100

    Start of App

--- demo/src/main/java/com/example/demo/dao/FakePersonInfo.java
+++ demo/src/main/java/com/example/demo/dao/FakePersonInfo.java
@@ -3,8 +3,10 @@ package com.example.demo.dao;
 import com.example.demo.model.Person;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository("fakeDAO")
@@ -21,4 +23,20 @@ public class FakePersonInfo implements PersonDao{
         return DB;
     }
 
+    @Override
+    public int deletePersonById(UUID id) {
+        return 0;
+    }
+
+    @Override
+    public int updatePersonById(UUID id) {
+        return 0;
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return DB.stream()
+                .filter(person -> person.getId().equals(id))
+                .findFirst();
+    }
 }
--- demo/src/main/java/com/example/demo/dao/PersonDao.java
+++ demo/src/main/java/com/example/demo/dao/PersonDao.java
@@ -3,6 +3,7 @@ package com.example.demo.dao;
 import com.example.demo.model.Person;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PersonDao {
@@ -13,4 +14,10 @@ public interface PersonDao {
         return insertPerson(id, person);
     }
     List<Person> selectAllPeople();
+
+    int deletePersonById(UUID id);
+
+    int updatePersonById(UUID id);
+
+    Optional<Person> selectPersonById(UUID id);
 }
