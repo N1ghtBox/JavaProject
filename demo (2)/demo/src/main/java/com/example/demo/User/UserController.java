84548a32eee245faa690ec632dfb61d89a5ccd1c
KacperGielda <mateuszhoch834@gmail.com>
2021-03-11 15:01:19 +0100

    Getting data from website

--- demo (2)/demo/src/main/java/com/example/demo/User/UserController.java
+++ demo (2)/demo/src/main/java/com/example/demo/User/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping(path = "hello")
+@RequestMapping(path = "user")
 @RestController
 public class UserController {
     private final UserService userService;
